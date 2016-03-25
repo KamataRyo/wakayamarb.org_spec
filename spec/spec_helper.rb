c36a8380b5fe8c0972132b0cf747f1381c5aed2d
KamataRyo <mugil.cephalus+github@gmail.com>
2016-03-26 08:26:04 +0900

    Set question for ssh username

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,24 +1,33 @@
 require 'serverspec'
 require 'net/ssh'
 
-set :backend, :ssh
-
-if ENV['ASK_SUDO_PASSWORD']
+if ENV['ASK_SSH_USERNAME'] || ENV['ASK_SUDO_PASSWORD']
   begin
     require 'highline/import'
   rescue LoadError
     fail "highline is not available. Try installing it."
   end
-  set :sudo_password, ask("Enter sudo password: ") { |q| q.echo = false }
+end
+
+set :backend, :ssh
+ssh_username = ''
+
+
+if ENV['ASK_SSH_USERNAME']
+  ssh_username = ask("Enter SSH username: ") { |q| q.echo = false }
+else
+  ssh_username = ENV['SSH_USERNAME'] || Etc.getlogin
+end
+
+if ENV['ASK_SUDO_PASSWORD']
+  set :sudo_password, ask("Enter SSH user's sudo password: ") { |q| q.echo = false }
 else
   set :sudo_password, ENV['SUDO_PASSWORD']
 end
 
 host = ENV['TARGET_HOST']
-
 options = Net::SSH::Config.for(host)
-
-options[:user] ||= Etc.getlogin
+options[:user] ||= ssh_username
 
 set :host,        options[:host_name] || host
 set :ssh_options, options
@@ -28,7 +37,7 @@ set :ssh_options, options
 
 
 # Set environment variables
-# set :env, :LANG => 'C', :LC_MESSAGES => 'C' 
+# set :env, :LANG => 'C', :LC_MESSAGES => 'C'
 
 # Set PATH
 # set :path, '/sbin:/usr/local/sbin:$PATH'
